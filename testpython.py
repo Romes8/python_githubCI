fea82451428752c3686faccb6d1ca7624750e095
Romes8 <roman.zvoda@yahoo.com>
2021-10-18 12:10:13 +0200

    badtest

--- testpython.py
+++ testpython.py
@@ -1,8 +1,23 @@
-def Hello():
-    print("Hello world")
 
-def add_numbers(n1,n2):
-    return n1 + n2
+class Person():
+    def __init__(self,name):
+        self.name = name
+
+    def add_numbers(n1,n2):
+        return n1 + n2
+
+
+
+Roman = Person("Roman")
+Roman.add_numbers(4,2)
+
 
 def test_add_numbers():
-    assert add_numbers(4,2) == 6
+    assert Roman.add_numbers(4,2) == 6
+
+def test_name():
+    assert Roman.name == "Roman"
+        
+
+    
+
